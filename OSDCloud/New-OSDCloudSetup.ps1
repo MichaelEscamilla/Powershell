b1757597eb83b8405b7d398b7fa5bd267337b948
Michael Escamilla <MichaelEscamilla@protonmail.com>
2023-09-08 15:59:54 -0700

    Create New-OSDCloudSetup.ps1

---
+++ OSDCloud/New-OSDCloudSetup.ps1
@@ -0,0 +1,17 @@
+### Require that the scrip RunAsAdmin
+#Requires -RunAsAdministrator
+
+### Setup an OSDCloudTemplate: Include WinRE and the CU for SecureBoot Vulnerability
+$TemplateName = "WinRE-KB5026372"
+New-OSDCloudTemplate -Name $TemplateName -WinRE -CumulativeUpdate C:\_OSDCloud\KB5026372\windows11.0-kb5026372-x64_d2e542ce70571b093d815adb9013ed467a3e0a85.msu
+
+### Create an OSDCloudWorkspace - default is located 'C:\OSDCloud'
+# Just incase, set the template to the one created above
+Set-OSDCloudTemplate -Name $TemplateName
+New-OSDCloudWorkspace
+
+### Setup OSDCloudWinPE
+# Set a Brand Name to Display
+$BrandName = "Michael the Admin"
+# Will use the default wallpaper for now, and include some drivers
+Edit-OSDCloudWinPE -StartOSDCloudGUI -Brand "$($BrandName)" -UseDefaultWallpaper -CloudDriver WiFi,HP,USB
