7426e4504faaa15ac439cfffafb9755464f2615a
Michael Escamilla <MichaelEscamilla@protonmail.com>
2023-12-15 16:59:42 -0800

    Update New-OSDBuilderSetup.ps1

--- OSDBuilder/New-OSDBuilderSetup.ps1
+++ OSDBuilder/New-OSDBuilderSetup.ps1
@@ -19,9 +19,9 @@ $ScriptDate = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 
 # OSDBuilder Global Settings
 $Global:OSDBuilder_Global = [ordered]@{
-    ImportOSMediaEditionId   = "Enterprise"
-    ImportOSMediaUpdate     = $false
-    NewOSBuildSkipUpdates     = $true
+    ImportOSMediaEditionId = "Enterprise"
+    ImportOSMediaUpdate    = $false
+    NewOSBuildSkipUpdates  = $true
 }
 $OSDBuilderjson = New-Item -Path "$($env:ProgramData)\OSDeploy\OSDBuilder.json" -Force
 $Global:OSDBuilder_Global | ConvertTo-Json -Depth 10 | Out-File -FilePath $($OSDBuilderjson.FullName) -Force
@@ -33,22 +33,24 @@ Get-OSDBuilder -Initialize -Verbose
 #OSDBuilder -SetPath E:\_OSDBuilder
 
 # Mount ISO for Import
-$ISOPath = "E:\_OSDBuilder\_ISO\SW_DVD9_Win_Pro_11_23H2_64BIT_English_Pro_Ent_EDU_N_MLF_X23-59562.ISO"
-$ISOPath = "C:\_ISO\SW_DVD9_Win_Server_STD_CORE_2022_2108.27_64Bit_English_DC_STD_MLF_X23-64869.ISO"
+$ISOPath = "D:\_ISO\Windows\SW_DVD9_Win_Pro_11_23H2_64BIT_English_Pro_Ent_EDU_N_MLF_X23-59562.ISO"
 $DiskImage_Mount = Mount-DiskImage -ImagePath $ISOPath
 
 # Import-OSMedia - Should Auto Import the 'Enterprise' index based on our Setting above
-Import-OSMedia -SkipGrid -BuildNetFX
+$OSImport = Import-OSMedia -SkipGrid -BuildNetFX
 
-#
+# Dismount ISO
+$DiskImage_Mount | Dismount-DiskImage
+
+# Avaialble OSMedia
 $OSMedia = Get-OSMedia -GridView
 
-# Copy WIM to ConfigMgr OS Image Path
+<# # Copy WIM to ConfigMgr OS Image Path
 $FileNameOriginal = "install.wim"
 $FileNameNew = "$($TaskCustomname)_$($ScriptDate).wim"
 $FilePathSource = ""
 $FilePathDestination = "\\SCVM2\SourcePackages\OSD\OperatingSystemImages"
-Copy-Item -Path $(Join-Path $FilePathSource $FileNameOriginal) -Destination $(Join-Path $FilePathDestination $FileNameNew)
+Copy-Item -Path $(Join-Path $FilePathSource $FileNameOriginal) -Destination $(Join-Path $FilePathDestination $FileNameNew) #>
 
 # Get
 
