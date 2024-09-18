785ffca5261eedbc31ab037c91587789e0ef83a9
Michael Escamilla <126358188+MichaelEscamilla@users.noreply.github.com>
2024-09-18 15:08:40 -0700

    Update

--- Lab/Build-LabADObjects.ps1
+++ Lab/Build-LabADObjects.ps1
@@ -54,7 +54,8 @@ function Build-LabADObjects {
                     Write-Output "Group '$groupName' already exists. Updating Information"
                 }
                 else {
-                    New-ADGroup -Name $groupName -SamAccountName $groupName -DisplayName $groupName -GroupCategory Security -GroupScope Global -Path $OUName -Description $groupDescription -ErrorAction Stop
+                    $OUPath = "OU=$OUName" + "DC=$($DomainName -replace '\.', ',DC=')"
+                    New-ADGroup -Name $groupName -SamAccountName $groupName -DisplayName $groupName -GroupCategory Security -GroupScope Global -Path "$($OUPath)" -Description $groupDescription -ErrorAction Stop
                     Write-Output "Group '$groupName' with description '$groupDescription' created successfully in Organizational Unit '$OUName'."
                 }
             }
